5b7e01e79d4258ff2b785830f0a0ea49902abe13
AnilRai600 <anil.rai600@gmail.com>
2020-11-26 11:28:22 +1100

    fist commit

---
+++ DAL/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Models
+{
+   public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Address { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public DateTime EnrolledDate { get; set; }
+
+
+    }
+}
